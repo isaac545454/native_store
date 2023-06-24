73b2a997aab2f022bb9799f741851f90cf9f804d
isaac545454 <isaac.newton1221@gmail.com>
2023-06-24 12:05:27 -0300

    feat: add files components and screens

--- App.tsx
+++ App.tsx
@@ -1,16 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native";
+import Login from "./screen/login";
 import style from "styled-components/native";
-
-const ButtonRed = style.Text`
-  color: red;
-`;
 
 export default function App() {
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <ButtonRed>aaaaaaaaaa</ButtonRed>
-    </View>
+    <SafeAreaView>
+      <Login />
+    </SafeAreaView>
   );
 }
